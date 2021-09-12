4d2aed31a421aca75377abbef7578a1719de8542
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2021-09-12 22:45:08 +0300

    #3 Some tweaks before starting to create pipeline.

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,25 +1,24 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>Under construction.</p>
+      <a
+        className="App-link"
+        href="https://github.com/CodeWell-fi/website"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        This site was made with Azure, Pulumi, React, and lots of tea
+        <span role="img" aria-label="tea">
+          🍵
+        </span>
+      </a>
+    </header>
+  </div>
+);
 
 export default App;
