38abe7ad48c48ce1e0510580879dc70c632de143
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2021-09-14 19:21:17 +0300

    #3 Updating website unit tests.

--- website/src/__test__/App.spec.tsx
+++ website/src/__test__/App.spec.tsx
@@ -1,8 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import App from "../App";
 
-test("renders learn react link", () => {
+test("renders footer", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(
+    /This site was made with Azure, Pulumi, React, and lots of/i,
+  );
   expect(linkElement).toBeInTheDocument();
 });
