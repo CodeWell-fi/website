abe205584e94610c97e3333056523b24f8f407a1
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2021-10-03 19:47:21 +0300

    #31 Adding code to create CNAME record to zone, i…
    …f so specified.

--- infrastructure/src/input.ts
+++ infrastructure/src/input.ts
@@ -1,11 +1,44 @@
 import * as t from "io-ts";
 import * as validation from "@data-heaving/common-validation";
 
+const zoneInfo = t.type(
+  {
+    resourceGroupName: validation.nonEmptyString,
+    zoneName: validation.nonEmptyString,
+  },
+  "TargetDNSZoneInfo",
+);
+
+export type ZoneInfo = t.TypeOf<typeof zoneInfo>;
+
+const domainNameList = t.array(
+  t.union([
+    validation.nonEmptyString,
+    t.type(
+      {
+        zone: zoneInfo,
+        relativeName: validation.nonEmptyString,
+      },
+      "ZoneJoinInfo",
+    ),
+  ]),
+  "DomainNameList",
+);
+
 export const configuration = t.type({
   resourceGroupName: validation.nonEmptyString,
   organization: validation.nonEmptyString,
   environment: validation.nonEmptyString,
-  domainNames: t.array(validation.nonEmptyString),
+  domainNames: t.union(
+    [
+      domainNameList,
+      t.type({
+        defaultZone: zoneInfo,
+        domains: domainNameList,
+      }),
+    ],
+    "DomainNameJoinConfiguration",
+  ),
   httpsEnabled: t.boolean,
 });
 
