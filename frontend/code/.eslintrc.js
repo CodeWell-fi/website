bb719c195cc74d4063f21abab9a07193d350fa84
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2021-10-18 22:49:57 +0300

    #32 Added necessary scripts to embed version info…
    …rmation to website. Also made footer same width as header.

--- frontend/code/.eslintrc.js
+++ frontend/code/.eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
     // See https://github.com/prettier/eslint-config-prettier/blob/main/CHANGELOG.md#version-800-2021-02-21
     "plugin:prettier/recommended",
   ],
-  plugins: ["prettier"],
+  plugins: ["prettier", "react"],
   env: {
     node: true,
     es2020: true
@@ -23,6 +23,9 @@ module.exports = {
     sourceType: "module",
     ecmaVersion: 2020,
     tsconfigRootDir: __dirname,
+    "ecmaFeatures": {
+      "jsx": true
+    }
   },
   rules: {
     "prettier/prettier": "error",
@@ -40,6 +43,9 @@ module.exports = {
         paths: ["src"],
         extensions: [".ts", ".tsx"] // Add .js, .jsx if needed
       }
+    },
+    "react": {
+      "version": "detect"
     }
   }
 };
