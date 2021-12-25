904353fa1cff9f49a04828602a8cb08a86bb13c1
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2021-12-25 13:50:00 +0200

    #21 Small change: Only expose WEBSITE_INFRA_CONFI…
    …G when needed.

--- .github/website-cd.sh
+++ .github/website-cd.sh
@@ -5,10 +5,10 @@ cd frontend/publish
 
 npm ci --ignore-scripts
 cp ../../infrastructure/website/src/naming.ts src
-export WEBSITE_INFRA_CONFIG="$(cat "config/config-${WEBSITE_ENV}.json")"
 npm run build
 npm run test
 
 if [[ -z "$1" ]] || [[ "${GITHUB_EVENT_NAME}" == "$1" ]]; then
+  export WEBSITE_INFRA_CONFIG="$(cat "config/config-${WEBSITE_ENV}.json")"
   npm run deploy
 fi
